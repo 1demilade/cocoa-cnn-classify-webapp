98a17639190ff7b8d7c00cf2349df4e035d3ddb5
David Adeyemi <subomiolubosi@gmail.com>
2025-06-16 12:01:30 +0100

    Test commit 1

--- assets/js/script.js
+++ assets/js/script.js
@@ -105,7 +105,7 @@ if (form) {
     formData.append('image', file);
 
     try {
-      const response = await fetch('https://5030-34-68-189-24.ngrok-free.app/predict', {
+      const response = await fetch('https://0766-34-55-45-0.ngrok-free.app/predict', {
         method: 'POST',
         body: formData,
       });
